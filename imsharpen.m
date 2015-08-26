ad1cc6f4c4de5852359b76b14fc2e84b44a0ec88
hsiaoyi0504 <hsiaoyi0504@gmail.com>
2015-08-27 00:37:22 +0800

    revise imsharpen

--- imsharpen.m
+++ imsharpen.m
@@ -1,13 +1,17 @@
-function [imgOut]=imsharpen(imgIn)
-%Input Image
-%Preallocate the matrices with zeros
+%sharpening the image by minus the laplacian (second derivative)
+function [imgOut]=imsharpen(imgIn,method)
 I1=imgIn;
+
+%Preallocate the matrices with zeros
 I=zeros(size(imgIn));
-I2=zeros(size(imgIn));
 
 %Filter Masks
-F1=[0 1 0;1 -4 1; 0 1 0];
-F2=[1 1 1;1 -8 1; 1 1 1];
+if method==1
+	F=[0 1 0; 1 -4 1; 0 1 0];
+elseif method==2
+	F=[1 1 1; 1 -8 1; 1 1 1];
+end
+
 
 %Padarray with zeros
 imgIn=padarray(imgIn,[1,1]);
@@ -16,9 +20,7 @@ imgIn=double(imgIn);
 %Implementation of the equation of Laplacian
 for i=1:size(imgIn,1)-2
     for j=1:size(imgIn,2)-2
-       
-        I(i,j)=sum(sum(F1.*imgIn(i:i+2,j:j+2)));
-       
+        I(i,j)=sum(sum(F.*imgIn(i:i+2,j:j+2)));
     end
 end
 %filtered Image
